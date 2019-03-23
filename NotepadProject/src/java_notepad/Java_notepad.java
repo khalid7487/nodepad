2438b6a576a985e9410724657d1e967c0ab1de0b
khalid7487 <khalid15-7487@diu.edu.bd>
2019-03-23 22:51:59 +0600

    initial

--- NotepadProject/src/java_notepad/Java_notepad.java
+++ NotepadProject/src/java_notepad/Java_notepad.java
@@ -1,11 +1,28 @@
-
 package java_notepad;
 
+import java.awt.*;
 import javax.swing.JFrame;
+import javax.swing.*;
 
+public class Java_notepad extends JFrame {
 
-public class Java_notepad extends JFrame{
+    JTextArea mainarea;
+
+    public Java_notepad() {
+        initComponent();
+    }
+
+    private void initComponent() {
+        mainarea = new JTextArea();
+        getContentPane().add(mainarea);
+        getContentPane().add(new JScrollPane(mainarea),BorderLayout.CENTER);
+        setTitle("Untitled NotePad");
+        setSize(800,600);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setVisible(true);
+    }
+
     public static void main(String[] args) {
-        
+       Java_notepad jn=new Java_notepad();
     }
 }
