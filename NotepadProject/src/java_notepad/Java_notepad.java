b34157ee30ac3901114ea9c66b3982133c5e7d1d
khalid7487 <khalid15-7487@diu.edu.bd>
2019-03-23 23:09:32 +0600

    add menu bar  to jframe

--- NotepadProject/src/java_notepad/Java_notepad.java
+++ NotepadProject/src/java_notepad/Java_notepad.java
@@ -1,12 +1,14 @@
 package java_notepad;
 
 import java.awt.*;
-import javax.swing.JFrame;
 import javax.swing.*;
 
 public class Java_notepad extends JFrame {
 
     JTextArea mainarea;
+    JMenuBar mbar;
+    JMenu mnuFile,mnuEdit,mnuFormat,mnuHelp;
+    JMenuItem itemNew,itemOpen,itemSave;
 
     public Java_notepad() {
         initComponent();
@@ -18,6 +20,30 @@ public class Java_notepad extends JFrame {
         getContentPane().add(new JScrollPane(mainarea),BorderLayout.CENTER);
         setTitle("Untitled NotePad");
         setSize(800,600);
+        //menubar
+        mbar=new JMenuBar();
+        //menu
+        mnuFile=new JMenu("File");
+        mnuEdit=new JMenu("Edit");
+        mnuFormat=new JMenu("Format");
+        mnuHelp=new JMenu("Help");
+        //menuitem
+        itemNew=new JMenuItem("New");
+        itemOpen=new JMenuItem("Open");
+        itemSave=new JMenuItem("Save");
+        //add menu item
+        mnuFile.add(itemNew);
+        mnuFile.add(itemOpen);
+        mnuFile.add(itemSave);
+        //add menu item to menu bar
+        mbar.add(mnuFile);
+        mbar.add(mnuEdit);
+        mbar.add(mnuFormat);
+        mbar.add(mnuHelp);
+        //add menubar to frame
+        setJMenuBar(mbar);
+        
+        
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setVisible(true);
     }
